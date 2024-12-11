8bdfadf7c0395f8476b662e82fb90dfb2660a922
Ratikanta Behera <ratikantabehera360@gmail.com>
2024-12-11 21:29:18 +0530

    About section and contact add

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Services from './Components/Services';
 import ContactForm from './Components/ContactForm';
+import AboutUs from './Components/AboutUs';
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <AboutUs/>
       <Services />
       <ContactForm />
+      
     </div>
   );
 }
---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import aboutus from '../assets/aboutus.png';  // Replace with your image path
+
+function AboutUs() {
+  return (
+    <section className="bg-black text-white py-12">
+  <div className="container mx-auto text-center"> {/* Add text-center here */}
+    <h2 className="text-3xl font-bold mb-4 text-center">About Us</h2> {/* text-center here */}
+    <p className="text-lg mb-8 text-center"> {/* text-center here */}
+      We are a financial advisory company committed to helping individuals achieve financial freedom through expert advice, personalized strategies, and a client-first approach.
+    </p>
+  </div>
+  <div className="container mx-auto">
+    <img src={aboutus} alt="About Us" className="w-full h-auto" />
+  </div>
+</section>
+
+  );
+}
+
+export default AboutUs;
