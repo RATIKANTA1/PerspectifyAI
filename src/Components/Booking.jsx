87548d941092dca297c1e4c7c60d4eade8d194cc
Rdhurua <149248792+Rdhurua@users.noreply.github.com>
2024-12-12 18:50:21 +0530

    Rinku (#8)
    * navbar,aboutUs updated
    
    * carousel set-controller,service-text set

--- src/Components/Booking.jsx
+++ src/Components/Booking.jsx
@@ -34,105 +34,134 @@ function Booking() {
   };
 
   return (
-    <section id="book-appointment" className="bg-black text-white py-12">
-      <div className="container mx-auto max-w-5xl flex justify-between">
-        {/* Left: Booking Form */}
-        <div className="w-1/2 p-6 bg-gray-800 rounded shadow-md">
-          <h2 className="text-3xl font-bold text-center text-blue-600 mb-8">Request a New Appointment</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label className="block text-gray-400">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-600 rounded bg-gray-900 text-white"
-                placeholder="Your Name"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-400">Email</label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-600 rounded bg-gray-900 text-white"
-                placeholder="Your Email"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-400">Phone</label>
-              <input
-                type="tel"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-600 rounded bg-gray-900 text-white"
-                placeholder="Your Phone Number"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-400">Date</label>
-              <input
-                type="date"
-                name="date"
-                value={formData.date}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-600 rounded bg-gray-900 text-white"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-400">Time</label>
-              <input
-                type="time"
-                name="time"
-                value={formData.time}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-600 rounded bg-gray-900 text-white"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-400">Message</label>
-              <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                className="w-full p-3 border border-gray-600 rounded bg-gray-900 text-white"
-                placeholder="Your Message"
-                rows="4"
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-600 text-white p-3 rounded w-full hover:bg-blue-500"
-            >
-              Submit
-            </button>
-          </form>
+    <section id="book-appointment" className="bg-black text-white py-12 px-4">
+  <div className="container mx-auto max-w-5xl flex flex-col md:flex-row items-start justify-between gap-8">
+    {/* Left: Booking Form */}
+    <div className="w-full md:w-1/2 p-6 bg-gray-900 rounded-lg shadow-lg">
+      <h2 className="text-3xl font-bold text-center text-blue-500 mb-8">
+        Request a New Appointment
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block  text-gray-400 text-sm font-semibold mb-2">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Your Name"
+            required
+          />
         </div>
-
-         {/* Right Side: How does it work Section */}
-      <div className="w-1/2 p-8 bg-gray-800 text-gray-300 rounded-lg shadow-lg ml-8">
-        <h2 className="text-2xl font-bold mb-4">How does it work?</h2>
-        <ul className="list-disc pl-5 space-y-2">
-          <li>Take a financial literacy assessment to understand your strengths and areas for improvement.</li>
-          <li>Connect with your personalised AI mentor for tailored guidance and support.</li>
-          <li>Engage continuously with interactive lessons and tools to enhance your financial skills.</li>
-          <li>Connect with highly credited financial advisers for expert advice.</li>
-        </ul>
-        <p className="mt-4 text-lg font-semibold">
-          Perspectify is a social enterprise committed to providing bespoke financial education through advanced AI technology.
-        </p>
-      </div>
+        <div className="mb-4">
+          <label className="block text-gray-400 text-sm font-semibold mb-2">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Your Email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-400 text-sm font-semibold mb-2">
+            Phone
+          </label>
+          <input
+            type="tel"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Your Phone Number"
+            required
+          />
+        </div>
+        {/* <div className="mb-4">
+          <label className="block text-gray-400 text-sm font-semibold mb-2">
+            Date
+          </label>
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+            className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div> */}
+        <div className="mb-4">
+          <label className="block text-gray-400 text-sm font-semibold mb-2">
+            Time
+          </label>
+          <input
+            type="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+            className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-400 text-sm font-semibold mb-2">
+            Message
+          </label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            className="w-full p-3 border border-gray-700 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Your Message"
+            rows="4"
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-3 rounded-lg w-full hover:bg-blue-500 transition-colors"
+        >
+          Submit
+        </button>
+      </form>
     </div>
-    </section>
+
+    {/* Right Side: How does it work Section */}
+    <div className="w-full md:w-1/2 p-8 bg-gray-900  text-gray-300 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-6 text-blue-400">
+        How does it work?
+      </h2>
+      <ul className="list-disc pl-5 space-y-4 text-gray-300">
+        <li className='text-lg'>
+          Take a financial literacy assessment to understand your strengths and
+          areas for improvement.
+        </li>
+        <li className='text-lg'>
+          Connect with your personalized AI mentor for tailored guidance and
+          support.
+        </li>
+        <li className='text-lg'>
+          Engage continuously with interactive lessons and tools to enhance your
+          financial skills.
+        </li>
+        <li className='text-lg'>
+          Connect with highly credited financial advisers for expert advice.
+        </li>
+      </ul>
+      <p className="mt-6 text-lg font-semibold text-gray-100">
+        Perspectify is a social enterprise committed to providing bespoke
+        financial education through advanced AI technology.
+      </p>
+    </div>
+  </div>
+</section>
+
   );
 }
 
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -10,18 +10,18 @@ import service3 from '../assets/services3.jpg'
           <div className="grid md:grid-cols-3 gap-8">
             <div className="bg-gray-900 p-6 rounded-lg shadow-lg hover:bg-gray-800 duration-300">
               <img src={service1} alt="Service 1" className="w-full h-42 object-cover rounded mb-4" />
-              <h3 className="text-xl font-semibold text-white">Financial Planning</h3>
-              <p className="mt-4 text-white text-md text-justify leading-relaxed">Tailored strategies to help you manage your money and investments effectively. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi natus perferendis, nihil optio quia culpa impedit necessitatibus cumque! Et ipsam assumenda cupiditate.</p>
+              <h3 className="text-xl font-semibold text-blue-500 hover:text-700">Financial Planning</h3>
+              <p className="mt-4 text-gray-300 text-lg  leading-normal  tracking-tight text-justify">Tailored strategies to help you manage your money and investments effectively. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi natus perferendis, nihil optio quia culpa impedit necessitatibus cumque! Et ipsam assumenda cupiditate.</p>
             </div>
             <div className="bg-gray-900 p-6 rounded-lg shadow-lg hover:bg-gray-800 duration-300">
               <img src={service2} alt="Service 2" className="w-full h-42 object-cover rounded mb-4" />
-              <h3 className="text-xl font-semibold text-white">Investment Advice</h3>
-              <p className="mt-4 text-white text-justify leading-relaxed">Expert insights into maximizing your investment returns safely and sustainably. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatem eaque temporibus aut mollitia minus delectus ipsa sequi consectetur cumque. Reiciendis fugit neque voluptatum?</p>
+              <h3 className="text-xl font-semibold text-blue-500 hover:text-700">Investment Advice</h3>
+              <p className="mt-4 text-gray-300 text-lg text-justify leading-normal tracking-tight ">Expert insights into maximizing your investment returns safely and sustainably. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatem eaque temporibus aut mollitia minus delectus ipsa sequi consectetur cumque. Reiciendis fugit neque voluptatum?</p>
             </div>
             <div className="bg-gray-900 p-6 rounded-lg shadow-lg hover:bg-gray-800 duration-300">
               <img src={service3} alt="Service 3" className="w-full h-42 object-cover rounded mb-4" />
-              <h3 className="text-xl font-semibold text-white">Debt Management</h3>
-              <p className="mt-4 text-white text-justify leading-relaxed">Personalized plans to help you manage and reduce your debts more efficiently. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat velit exercitationem aliquam repellendus quaerat neque. Minus adipisci dignissimos perferendis similique, eaque odit magnam inventore!</p>
+              <h3 className="text-xl font-semibold text-blue-500 ">Debt Management</h3>
+              <p className="mt-4 text-gray-300 text-lg text-justify leading-normal tracking-tight ">Personalized plans to help you manage and reduce your debts more efficiently. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat velit exercitationem aliquam repellendus quaerat neque. Minus adipisci dignissimos perferendis similique, eaque odit magnam inventore!</p>
             </div>
           </div>
         </div>
