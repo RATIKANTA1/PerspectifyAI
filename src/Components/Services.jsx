cbcc3aa30bc1f5409b3e4f47051289e79def857d
Rdhurua <149248792+Rdhurua@users.noreply.github.com>
2024-12-12 13:55:32 +0530

    navbar,aboutUs updated (#6)

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -4,24 +4,24 @@ import service3 from '../assets/services3.jpg'
 
   function Services() {
     return (
-      <section id='services' className="bg-black py-12">
+      <section id='services' className="bg-black py-16 px-4">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold text-blue-600 mb-8">Our Services</h2>
           <div className="grid md:grid-cols-3 gap-8">
-            <div className="bg-gray-500 p-6 rounded-lg shadow-lg">
+            <div className="bg-gray-900 p-6 rounded-lg shadow-lg hover:bg-gray-800 duration-300">
               <img src={service1} alt="Service 1" className="w-full h-42 object-cover rounded mb-4" />
-              <h3 className="text-xl font-semibold">Financial Planning</h3>
-              <p className="mt-4">Tailored strategies to help you manage your money and investments effectively. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi natus perferendis, nihil optio quia culpa impedit necessitatibus cumque! Et ipsam assumenda cupiditate.</p>
+              <h3 className="text-xl font-semibold text-white">Financial Planning</h3>
+              <p className="mt-4 text-white text-md text-justify leading-relaxed">Tailored strategies to help you manage your money and investments effectively. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi natus perferendis, nihil optio quia culpa impedit necessitatibus cumque! Et ipsam assumenda cupiditate.</p>
             </div>
-            <div className="bg-gray-500 p-6 rounded-lg shadow-lg">
+            <div className="bg-gray-900 p-6 rounded-lg shadow-lg hover:bg-gray-800 duration-300">
               <img src={service2} alt="Service 2" className="w-full h-42 object-cover rounded mb-4" />
-              <h3 className="text-xl font-semibold">Investment Advice</h3>
-              <p className="mt-4">Expert insights into maximizing your investment returns safely and sustainably. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatem eaque temporibus aut mollitia minus delectus ipsa sequi consectetur cumque. Reiciendis fugit neque voluptatum?</p>
+              <h3 className="text-xl font-semibold text-white">Investment Advice</h3>
+              <p className="mt-4 text-white text-justify leading-relaxed">Expert insights into maximizing your investment returns safely and sustainably. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptatem eaque temporibus aut mollitia minus delectus ipsa sequi consectetur cumque. Reiciendis fugit neque voluptatum?</p>
             </div>
-            <div className="bg-gray-500 p-6 rounded-lg shadow-lg">
+            <div className="bg-gray-900 p-6 rounded-lg shadow-lg hover:bg-gray-800 duration-300">
               <img src={service3} alt="Service 3" className="w-full h-42 object-cover rounded mb-4" />
-              <h3 className="text-xl font-semibold">Debt Management</h3>
-              <p className="mt-4">Personalized plans to help you manage and reduce your debts more efficiently. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat velit exercitationem aliquam repellendus quaerat neque. Minus adipisci dignissimos perferendis similique, eaque odit magnam inventore!</p>
+              <h3 className="text-xl font-semibold text-white">Debt Management</h3>
+              <p className="mt-4 text-white text-justify leading-relaxed">Personalized plans to help you manage and reduce your debts more efficiently. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat velit exercitationem aliquam repellendus quaerat neque. Minus adipisci dignissimos perferendis similique, eaque odit magnam inventore!</p>
             </div>
           </div>
         </div>
